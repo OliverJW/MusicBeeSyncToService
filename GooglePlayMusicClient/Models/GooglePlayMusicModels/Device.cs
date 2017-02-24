658ecffb39729292c702527c18adfdd842a427f4
mitchhymel <mjhymel111@gmail.com>
2017-02-23 23:21:30 -0800

    Adding back GPM client instead of consuming via NuGet.
    Because use of the GPM client is technically a violation of Google's terms of use, the NuGet package should technically not be allowed in the Gallery. I have made it unlisted for now as there is no easy way to completely remove it from the Gallery.

---
+++ GooglePlayMusicClient/Models/GooglePlayMusicModels/Device.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace GooglePlayMusicAPI
+{
+    [DataContract]
+    public class Device
+    {
+        [DataMember(Name = "kind")]
+        public string Kind { get; set; }
+        [DataMember(Name = "friendlyName")]
+        public string FriendlyName { get; set; }
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+        [DataMember(Name = "lastAccessedTimeMs")]
+        public string LastAccessedTimeMs { get; set; }
+        [DataMember(Name = "type")]
+        public DeviceType Type { get; set; }
+        [DataMember(Name = "smartPhone")]
+        public bool SmartPhone { get; set; }
+
+        [DataContract]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public enum DeviceType
+        {
+            [DataMember(Name = "ANDROID")]
+            Android,
+
+            [DataMember(Name = "IOS")]
+            IOS,
+
+            [DataMember(Name = "DESKTOP_APP")]
+            [EnumMember(Value = "DESKTOP_APP")]
+            DesktopApp
+        }
+    }
+}
